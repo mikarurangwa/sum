6a52693e341df19813f20c03d31cba5601521561
mikarurangwa <r.mika@alustudent.com>
2024-07-20 01:46:49 +0200

    Update student.py

--- student.py
+++ student.py
@@ -8,14 +8,4 @@ class Student:
     def calculate_GPA(self):
         total_credits = sum(course['credits'] for course in self.courses_registered)
         if total_credits == 0:
-            self.GPA = 0.0
-        else:
-            total_points = sum(course['grade'] * course['credits'] for course in self.courses_registered)
-            self.GPA = total_points / total_credits
-    
-    def register_for_course(self, course, grade):
-        self.courses_registered.append({'course': course, 'grade': grade, 'credits': course.credits})
-    
-    def update_info(self, new_email, new_names):
-        self.email = new_email
-        self.names = new_names
+            self.GPA = 0.
