71debf37a9d0aced6dcbc3f3fc7eff612f0d9694
mikarurangwa <r.mika@alustudent.com>
2024-07-20 01:21:01 +0200

    Update course.py

--- course.py
+++ course.py
@@ -1,1 +1,10 @@
-
+class Course:
+    def __init__(self, name, trimester, credits):
+        self.name = name
+        self.trimester = trimester
+        self.credits = credits
+    
+    def update_info(self, new_name, new_trimester, new_credits):
+        self.name = new_name
+        self.trimester = new_trimester
+        self.credits = new_credits
