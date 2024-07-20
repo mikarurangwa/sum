a4c8b4e6899622ec1aca43e22b359101ebdfefb5
mikarurangwa <r.mika@alustudent.com>
2024-07-20 04:26:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Course:
     def __init__(self, name, trimester, credits):
         self.name = name
         self.trimester = trimester
-        self.credits = credits
+        self.credits = int(credits)
 
     def __str__(self):
         return f"{self.name};{self.trimester};{self.credits}"
