6783f7d86427d8d6272ea9a5b1b4ff549e5340da
mikarurangwa <r.mika@alustudent.com>
2024-07-20 04:44:19 +0200

    Delete course.py

--- course.py
+++
@@ -1,6 +0,0 @@
-class Course:
-    def __init__(self, name, trimester, credits):
-        self.name = name
-        self.trimester = trimester
-        self.credits = credits
-        self.grade = 0  # This will be set when registering grades for the student
